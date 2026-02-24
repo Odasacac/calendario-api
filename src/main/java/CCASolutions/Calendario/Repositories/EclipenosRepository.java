8f4d93169370bc26378e484a48d4efdf78144fa2
CarlosCA <carloscasadoaguilera@gmail.com>
2026-02-24 16:42:05 +0100

    V1: getDateOFromDateVAU y getDateVAUFromDateO

--- src/main/java/CCASolutions/Calendario/Repositories/EclipenosRepository.java
+++
@@ -1,9 +0,0 @@
-package CCASolutions.Calendario.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import CCASolutions.Calendario.Entities.EclipenosEntity;
-
-public interface EclipenosRepository extends JpaRepository <EclipenosEntity, Long>{
-
-}
--- src/main/java/CCASolutions/Calendario/Services/EclipenosService.java
+++
@@ -1,6 +0,0 @@
-package CCASolutions.Calendario.Services;
-
-public interface EclipenosService {
-	
-	public abstract String poblateEclipenos();
-}
