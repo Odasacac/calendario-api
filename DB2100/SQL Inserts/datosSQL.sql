75f9f11cbc7581d63ec9c31981b29444d49f097a
CarlosCA <carloscasadoaguilera@gmail.com>
2026-06-29 22:20:00 +0200

    Incluido el dump de la base de datos

--- DB2100/SQL Inserts/datosSQL.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO calendar_db.datos (concepto,valor) VALUES
-	 ('PW','$2a$10$s1bbK5wuB6IBV3fcmBTe5uf3pqDv.DDbQekH9K.P92et3/iZIFsJS'),
-	 ('ASYEF','https://opale.imcce.fr/api/v1/phenomena/equinoxessolstices/399?year={{YYYY}}&nbd={{NNNN}}'),
-	 ('YLP','https://opale.imcce.fr/api/v1/phenomena/moonphases?year={{YYYY}}'),
-	 ('LEPY','https://opale.imcce.fr/api/v1/phenomena/eclipses/301/{{YYYY}}'),
-	 ('SEPY','https://opale.imcce.fr/api/v1/phenomena/eclipses/10/{{YYYY}}'),
-	 ('APG','https://opale.imcce.fr/api/v1/phenomena/distances?date={{YYYY-MM-DD}}&nbd={{DDDD}}&bodies=399,301&calendar=gregorian');
--- DB2100/SQL Inserts/weeksSQL.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO calendar_db.weeks (name,week_of_month) VALUES
-	 ('Primana',1),
-	 ('Segana',2),
-	 ('Terana',3),
-	 ('Curana',4),
-	 ('Limana',5),
-	 ('Nomana',0);
