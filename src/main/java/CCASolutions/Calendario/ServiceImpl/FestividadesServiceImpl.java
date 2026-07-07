6a4ff65be1f594aed0eb461a2361c3435c7024e1
CarlosCA <carloscasadoaguilera@gmail.com>
2026-07-08 00:37:36 +0200

    Reordenado el orden de las festividades

--- src/main/java/CCASolutions/Calendario/ServiceImpl/FestividadesServiceImpl.java
+++ src/main/java/CCASolutions/Calendario/ServiceImpl/FestividadesServiceImpl.java
@@ -28,26 +28,31 @@ public class FestividadesServiceImpl implements FestividadesService {
 			
 			List<FestividadesEntity> festividadParaDDB = new ArrayList<>();
 			
-			festividadParaDDB.add(this.crearFestividad("CE", "Cambio de eclípeno invernal nuevo", false));
 			festividadParaDDB.add(this.crearFestividad("CEAR", "Cambio de eclípeno apofasal remoto", false));
+			
+			festividadParaDDB.add(this.crearFestividad("CE", "Cambio de eclípeno invernal nuevo", false));			
+			festividadParaDDB.add(this.crearFestividad("CMAR", "Cambio de métono apofasal remoto", false));
+			
 			festividadParaDDB.add(this.crearFestividad("CMF", "Cambio de métono invernal fasal", false));
 			festividadParaDDB.add(this.crearFestividad("CMA", "Cambio de métono invernal apórico", false));
-			festividadParaDDB.add(this.crearFestividad("CMAR", "Cambio de métono apofasal remoto", false));
+			
 			festividadParaDDB.add(this.crearFestividad("CA", "Cambio de año", false));
 			festividadParaDDB.add(this.crearFestividad("IA", "Inicio del primer mes del año", true));
+			festividadParaDDB.add(this.crearFestividad("MSI", "Midsison invernal", false));
+			
 			festividadParaDDB.add(this.crearFestividad("BP", "Bienvenida de la primavera", false));
+			festividadParaDDB.add(this.crearFestividad("MSP", "Midsison primaveral", false));
+			
 			festividadParaDDB.add(this.crearFestividad("MA", "Mitad del año", false));
+			festividadParaDDB.add(this.crearFestividad("MSE", "Midsison estival", false));
+			
 			festividadParaDDB.add(this.crearFestividad("DV", "Despedida del verano", true));
+			festividadParaDDB.add(this.crearFestividad("MSO", "Midsison otoñal", false));
+			
 			festividadParaDDB.add(this.crearFestividad("PO", "Entrada del otoño", false));
 			festividadParaDDB.add(this.crearFestividad("DA", "Despedida del año", true));
-			festividadParaDDB.add(this.crearFestividad("MSI", "Midsison invernal", false));
-			festividadParaDDB.add(this.crearFestividad("MSP", "Midsison primaveral", false));
-			festividadParaDDB.add(this.crearFestividad("MSE", "Midsison estival", false));
-			festividadParaDDB.add(this.crearFestividad("MSO", "Midsison otoñal", false));
-
 			
 			this.festividadesRepository.saveAll(festividadParaDDB);
-			
 		}
 		else {
 			System.out.println("Ya hay festividades en la base de datos.");
