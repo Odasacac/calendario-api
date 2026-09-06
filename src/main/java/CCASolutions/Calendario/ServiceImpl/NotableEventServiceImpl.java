6e1088f4d6efc14c38dbac6212386ea4717972e0
CarlosCA <carloscasadoaguilera@gmail.com>
2026-09-06 10:06:18 +0200

    Eliminadas importaciones no usadas

--- src/main/java/CCASolutions/Calendario/ServiceImpl/NotableEventServiceImpl.java
+++ src/main/java/CCASolutions/Calendario/ServiceImpl/NotableEventServiceImpl.java
@@ -3,8 +3,6 @@ package CCASolutions.Calendario.ServiceImpl;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.stereotype.Service;
 
